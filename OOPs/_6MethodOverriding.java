aaab3caa21cb3c3f02c7cf20e22d117ce0db5621
pushpa rajput <pr4541285@gmail.com>
2024-06-08 11:22:29 +0530

    create _6MethodOverriding.java file in oops folder

---
+++ OOPs/_6MethodOverriding.java
@@ -0,0 +1,37 @@
+package OOPs;
+
+class A {
+    public int a;
+
+    public int harry() {
+        return 4;
+    }
+
+    public void meth2() {
+        System.out.println("i am method 2 of class A");
+    }
+
+}
+
+class B extends A {
+    public void meth2() { // it override the method2 of class A when method 2 is call by class B's object
+        System.out.println(" i am method2 of class B ");
+    }
+
+    public void meth3() {
+        System.out.println(" i am method3 of class B ");
+    }
+
+}
+
+public class _6MethodOverriding {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        a.meth2();
+        b.meth2();
+        b.meth3();
+        System.out.println(b.harry());
+    }
+
+}
